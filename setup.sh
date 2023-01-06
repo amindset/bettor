30f6af43c75ef4b8a4d614cf6c6962940fe1af8f
Steve Harris <harriss305@gmail.com>
2023-01-05 19:08:12 -0500

    removed setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,4 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > .streamlit/config.toml
+
