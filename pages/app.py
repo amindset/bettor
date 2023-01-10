768096717b4aff2fb32d4b33e16dceaff7bbd72f
Steve Harris <harriss305@gmail.com>
2023-01-10 18:55:22 -0500

    made the main menu visible

--- pages/app.py
+++ pages/app.py
@@ -14,7 +14,6 @@ st.set_page_config(
 
 hide_st_style = """
             <style>
-            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
